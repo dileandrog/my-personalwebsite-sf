a39b375fa8ec3418536831901f4b6a920044044f
Gatti, Leandro <Leandro.Gatti@Honeywell.com>
2026-07-07 17:37:29 -0300

    Fix mobile nav auto-close behavior

--- static/js/site.js
+++ static/js/site.js
@@ -1,4 +1,23 @@
 $(function() {
+  function isMobileNav() {
+    return window.matchMedia('(max-width: 767px)').matches;
+  }
+
+  function closeMobileNav() {
+    if (!isMobileNav()) return;
+
+    var $toggle = $('.navigation-toggle[data-target="#navbar-1"]');
+    var $target = $('#navbar-1');
+
+    if ($target.length) {
+      $target.hide();
+    }
+
+    if ($toggle.length) {
+      $toggle.removeClass('navigation-toggle-show');
+    }
+  }
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -7,8 +26,26 @@ $(function() {
         $('html,body').animate({
           scrollTop: target.offset().top
         }, 1000);
+
+        closeMobileNav();
         return false;
       }
+    }
+  });
+
+  $('.item-nav a').on('click', function() {
+    closeMobileNav();
+  });
+
+  $(document).on('click', function(e) {
+    if (!isMobileNav()) return;
+    if ($(e.target).closest('.main-nav').length) return;
+    closeMobileNav();
+  });
+
+  $(document).on('keydown', function(e) {
+    if (e.key === 'Escape') {
+      closeMobileNav();
     }
   });
 });
